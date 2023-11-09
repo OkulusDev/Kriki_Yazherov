62788c9ae3e788910cfeb6e64650619505b76d4b
OkulusDev <x.okulus.dev@gmail.com>
2023-11-09 22:24:47 +0700

    крики во славу вождя

---
+++ header/Yazher.h
@@ -0,0 +1,105 @@
+/* Глупые русы придумали себе Ве крест крест! Тупые русы!
+Теперь вождь сказал, что это они у нас украли!
+
+Великий ЯщерЯщерЯщер язык готов бить чертовых русов */
+
+#ifndef Гнездо
+#define Гнездо
+
+#ifdef _WIN32
+#include <Windows.h>
+#define ящерский_говор SetConsoleOutputCP(1251)
+
+#endif
+
+#include <fstream>
+#include <iostream>
+
+
+#define вождь_ящер int main
+#define заложить using
+#define ящерские_земли std
+#define рявкнуть cout
+#define слушать cin
+#define кинуть return
+
+#define трудиться do
+#define биться_до while
+#define кружиться for
+
+#define ежели if
+#define а_то else
+
+#define сын_вождя class
+#define халупа_вождя struct
+#define перепись enum
+
+#define бить_русов break
+#define добить_русов continue
+#define ящерский_камень switch
+#define путь case
+#define базированно default
+
+// Русы поганные, путайтесь!
+#define ложь true
+#define правда false
+
+#define ловкость_ящерская endl
+#define нарожать new
+#define зарезать delete
+#define потерять nullptr
+#define ПУСТОТА NULL
+#define судьба random
+#define приказ_вождя const
+#define мощь pow
+#define получи_рус throw
+#define подлость_ящера exception
+#define читать_руны ifstream
+#define рисовать_руны ofstream
+#define руны fstream
+#define коллега friend
+#define окстись goto
+#define тьма_моя_лужа template
+#define говори typename
+#define королевский private
+#define общий public
+#define вождя_вещь protected
+#define вставить inline
+#define спермослав static
+#define тупой unsigned
+#define пытать_руса try
+#define поймать_хитрость catch
+#define племя namespace
+#define взломать open
+#define закупорить close
+#define суд_вождя auto
+#define ну static_cast
+#define копье_кидать peek
+
+#define поймать getline
+#define ящер_свой good
+#define рус_тупой fail
+#define смерть_руса eof
+#define смерть_руса eof
+#define приток_ящеров ostream
+#define отток_ящеров istream
+#define перепутать std::swap
+#define оторвать get
+
+заложить цыфра = int;
+заложить большой_цыфра = double;
+заложить малый_цыфра = float;
+заложить руна = char;
+заложить непонятные_руны = ящерские_земли::string;
+заложить пустоголовый = void;
+заложить дитя_ящерское = short;
+заложить ящер_длинношей = long;
+заложить великодлинный_ящер = long long;
+заложить размерчик = size_t;
+заложить дума = bool;
+
+
+перепись счёт_древних_русов{ круг = 0, палка = 1, две_руки = 2, три_плаки = 3, ноги_и_руки = 4,
+деньга = 5, ящерская_цыфра = 6, группа_ящеров = 7, два_круга = 8, русская_цыфра = 9, ящер_вождь_цыфра = 10 };
+
+#endif
